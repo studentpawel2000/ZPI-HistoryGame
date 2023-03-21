6b9b1c756e5d01e8fc55571a091a416ef5904b43
Paweł Cetnarowski <studentpawel2000@gmail.com>
2023-03-21 14:06:55 +0100

    Initial

---
+++ Library/Il2cppBuildCache/Android/armeabi-v7a/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,243 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void MainMenu::Start()
+extern void MainMenu_Start_m3B552CE289B1D7E5343961C8461C484EA61DB621 (void);
+// 0x00000002 System.Void MainMenu::LoadMainGame()
+extern void MainMenu_LoadMainGame_m5402475934CFD96670EDB5D0B4E0639E97A9263F (void);
+// 0x00000003 System.Void MainMenu::PlayGame()
+extern void MainMenu_PlayGame_m96A3CE2743BCB00B665AA3AC575AE4EBD9ED40B0 (void);
+// 0x00000004 System.Void MainMenu::GPS()
+extern void MainMenu_GPS_m9F7B4C2EAB5D3E3D857A39BCB3840D09E2FFF219 (void);
+// 0x00000005 System.Void MainMenu::QuitGame()
+extern void MainMenu_QuitGame_m9F32E266C6F6CE345067D062258362159D267030 (void);
+// 0x00000006 System.Void MainMenu::.ctor()
+extern void MainMenu__ctor_m4D77CEC8F91682A2D9492AE815F89B178BF9717D (void);
+// 0x00000007 System.Void Menu::Start()
+extern void Menu_Start_m50A1B94A03D9E9BE1E2B69E931A1E02943BB2F36 (void);
+// 0x00000008 System.Void Menu::Update()
+extern void Menu_Update_mDF23A59FAEDA72A8B115C4C3CF2ACCAFC6B415AD (void);
+// 0x00000009 System.Void Menu::.ctor()
+extern void Menu__ctor_m7EE9043A2E9DD126B6F69DD8AE3BC9CBDF6E2134 (void);
+// 0x0000000A System.Single ARFeatheredPlaneMeshVisualizer::get_featheringWidth()
+extern void ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m0AAD92A3D682C1DFC1B5630585550B6CAFBAFE32 (void);
+// 0x0000000B System.Void ARFeatheredPlaneMeshVisualizer::set_featheringWidth(System.Single)
+extern void ARFeatheredPlaneMeshVisualizer_set_featheringWidth_m4761377095817FA81908BC43FD08EF600F6CC231 (void);
+// 0x0000000C System.Void ARFeatheredPlaneMeshVisualizer::Awake()
+extern void ARFeatheredPlaneMeshVisualizer_Awake_m7FD9AC4C30D2F12A0A30CC3CAD9EC6C17B07C45D (void);
+// 0x0000000D System.Void ARFeatheredPlaneMeshVisualizer::OnEnable()
+extern void ARFeatheredPlaneMeshVisualizer_OnEnable_mEE0AFFC028F876DE841312FEB8CA15FEAD105FE1 (void);
+// 0x0000000E System.Void ARFeatheredPlaneMeshVisualizer::OnDisable()
+extern void ARFeatheredPlaneMeshVisualizer_OnDisable_m51213AC9D2DBD97C93CC41330F65D74661D5DC81 (void);
+// 0x0000000F System.Void ARFeatheredPlaneMeshVisualizer::ARPlane_boundaryUpdated(UnityEngine.XR.ARFoundation.ARPlaneBoundaryChangedEventArgs)
+extern void ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_m354D58D0B29B40FE6D3A6689590E271D9282323F (void);
+// 0x00000010 System.Void ARFeatheredPlaneMeshVisualizer::GenerateBoundaryUVs(UnityEngine.Mesh)
+extern void ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_m241EDA47E1C4DECA8CCBC61D8DF6EC300546CDF0 (void);
+// 0x00000011 System.Void ARFeatheredPlaneMeshVisualizer::.ctor()
+extern void ARFeatheredPlaneMeshVisualizer__ctor_mA27A238E88D5FBA54D4DDFE588080DD41B449A4C (void);
+// 0x00000012 System.Void ARFeatheredPlaneMeshVisualizer::.cctor()
+extern void ARFeatheredPlaneMeshVisualizer__cctor_mF5C9F857AC060614B46B9359B3F0859FBC309DF3 (void);
+// 0x00000013 System.Void GPSLocation::Start()
+extern void GPSLocation_Start_m11D560FBC61B4B9D9A3BB6253E16E8F1004E3824 (void);
+// 0x00000014 System.Collections.IEnumerator GPSLocation::GPSLoc()
+extern void GPSLocation_GPSLoc_mA47F0A51D66F6D7E585A5A60E6CD8F3E89DE22D3 (void);
+// 0x00000015 System.Void GPSLocation::UpdateGPSData()
+extern void GPSLocation_UpdateGPSData_m5703907876EFCB4D6C331A346FA90BAFA9FC75A6 (void);
+// 0x00000016 System.Void GPSLocation::.ctor()
+extern void GPSLocation__ctor_m6CB6A54C1BC5B56BADEE9FD50DA9F259F834D814 (void);
+// 0x00000017 System.Void GPSLocation/<GPSLoc>d__7::.ctor(System.Int32)
+extern void U3CGPSLocU3Ed__7__ctor_m12CD184E0688656F4629F334516458DFB5FC576C (void);
+// 0x00000018 System.Void GPSLocation/<GPSLoc>d__7::System.IDisposable.Dispose()
+extern void U3CGPSLocU3Ed__7_System_IDisposable_Dispose_m516EF9ED6831C68FA7AD3532B42DDA943C2F37A8 (void);
+// 0x00000019 System.Boolean GPSLocation/<GPSLoc>d__7::MoveNext()
+extern void U3CGPSLocU3Ed__7_MoveNext_m01660B82714AFA7A87161FB9686DDD9FB0441349 (void);
+// 0x0000001A System.Object GPSLocation/<GPSLoc>d__7::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CGPSLocU3Ed__7_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m679A24C73E5A917BC5C9C87C4DD7D26E9244545F (void);
+// 0x0000001B System.Void GPSLocation/<GPSLoc>d__7::System.Collections.IEnumerator.Reset()
+extern void U3CGPSLocU3Ed__7_System_Collections_IEnumerator_Reset_m961E137257682C39EAB056AE53D8D4331E473617 (void);
+// 0x0000001C System.Object GPSLocation/<GPSLoc>d__7::System.Collections.IEnumerator.get_Current()
+extern void U3CGPSLocU3Ed__7_System_Collections_IEnumerator_get_Current_mA80D190D4ACB35ACFE7D1F6EC23938E156E23189 (void);
+// 0x0000001D System.Void Interface::OpenMenuScene()
+extern void Interface_OpenMenuScene_mF08DA71F4E4B3535BFD7891D88F3D5445C670296 (void);
+// 0x0000001E System.Void Interface::.ctor()
+extern void Interface__ctor_m44EC2832625D427080CE1B16A4731F7FA804C09E (void);
+// 0x0000001F System.Void PlaceObjOnPlane::Awake()
+extern void PlaceObjOnPlane_Awake_m45954D39E5753EA730A55B8093179ED7F6023505 (void);
+// 0x00000020 System.Void PlaceObjOnPlane::Start()
+extern void PlaceObjOnPlane_Start_mB0F25C113A201B9CE495D542EAAB3FBD91DB8420 (void);
+// 0x00000021 System.Void PlaceObjOnPlane::Update()
+extern void PlaceObjOnPlane_Update_mA6218EDE3773A987D2F221993EAAEDC4F67C974A (void);
+// 0x00000022 System.Void PlaceObjOnPlane::UpdatePlacementPosition()
+extern void PlaceObjOnPlane_UpdatePlacementPosition_mCBBA7E82D7C37E2D0AACD59110310FB6BD31D57D (void);
+// 0x00000023 System.Void PlaceObjOnPlane::UpdatePlacementIndicator()
+extern void PlaceObjOnPlane_UpdatePlacementIndicator_m00585A1EF770BC41FEB45995D61B36AF58CB3B7E (void);
+// 0x00000024 System.Void PlaceObjOnPlane::PlaceObject()
+extern void PlaceObjOnPlane_PlaceObject_m4C114795091B34D32E2EB4F00281B683741DB0E0 (void);
+// 0x00000025 System.Void PlaceObjOnPlane::SetPrefabType(UnityEngine.GameObject)
+extern void PlaceObjOnPlane_SetPrefabType_mC235BB4386C172A157E90D3B4EC88CF294C5D10D (void);
+// 0x00000026 System.Collections.IEnumerator PlaceObjOnPlane::GPSLoc()
+extern void PlaceObjOnPlane_GPSLoc_m9654A549B2B472C38A26D5048D7476214DDB0741 (void);
+// 0x00000027 System.Void PlaceObjOnPlane::UpdateGPSData()
+extern void PlaceObjOnPlane_UpdateGPSData_mD6773D3D33C781DB91501E90209E253A794E64F5 (void);
+// 0x00000028 System.Void PlaceObjOnPlane::.ctor()
+extern void PlaceObjOnPlane__ctor_m12D5EB396F3C9B0ABF13A0E3D629A8032474FD7B (void);
+// 0x00000029 System.Void PlaceObjOnPlane::.cctor()
+extern void PlaceObjOnPlane__cctor_mC8BA14CA1F3CEFD48F1F828807A883A899950EDF (void);
+// 0x0000002A System.Void PlaceObjOnPlane/<GPSLoc>d__19::.ctor(System.Int32)
+extern void U3CGPSLocU3Ed__19__ctor_mDA9FB57B3E2F51FD82DF33C512580FBA9C05A0DC (void);
+// 0x0000002B System.Void PlaceObjOnPlane/<GPSLoc>d__19::System.IDisposable.Dispose()
+extern void U3CGPSLocU3Ed__19_System_IDisposable_Dispose_m1E83268494E311594B45DD3579BB94CC346FC948 (void);
+// 0x0000002C System.Boolean PlaceObjOnPlane/<GPSLoc>d__19::MoveNext()
+extern void U3CGPSLocU3Ed__19_MoveNext_mD2F43B64730C093D38F2A23A766675D29C778B8F (void);
+// 0x0000002D System.Object PlaceObjOnPlane/<GPSLoc>d__19::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CGPSLocU3Ed__19_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m54C179442D11BB1408A4CE9EC05BD6B5DEE75A1A (void);
+// 0x0000002E System.Void PlaceObjOnPlane/<GPSLoc>d__19::System.Collections.IEnumerator.Reset()
+extern void U3CGPSLocU3Ed__19_System_Collections_IEnumerator_Reset_mD168AD012ADD365412B0110FC686082D77105DC3 (void);
+// 0x0000002F System.Object PlaceObjOnPlane/<GPSLoc>d__19::System.Collections.IEnumerator.get_Current()
+extern void U3CGPSLocU3Ed__19_System_Collections_IEnumerator_get_Current_m3AFA995D705CC9476FFC0619A874E396BAA1B486 (void);
+// 0x00000030 System.Void placeArtefacts::Start()
+extern void placeArtefacts_Start_m60C00C300A875FA4D43D440ABC6284CFCBDDE29C (void);
+// 0x00000031 System.Void placeArtefacts::Update()
+extern void placeArtefacts_Update_mACC4E288299183ACBB44D1571F42F7EC90E1089E (void);
+// 0x00000032 System.Void placeArtefacts::.ctor()
+extern void placeArtefacts__ctor_m180BE66B9E59136D9083EC63B3F1D04D0EF8CEB5 (void);
+static Il2CppMethodPointer s_methodPointers[50] = 
+{
+	MainMenu_Start_m3B552CE289B1D7E5343961C8461C484EA61DB621,
+	MainMenu_LoadMainGame_m5402475934CFD96670EDB5D0B4E0639E97A9263F,
+	MainMenu_PlayGame_m96A3CE2743BCB00B665AA3AC575AE4EBD9ED40B0,
+	MainMenu_GPS_m9F7B4C2EAB5D3E3D857A39BCB3840D09E2FFF219,
+	MainMenu_QuitGame_m9F32E266C6F6CE345067D062258362159D267030,
+	MainMenu__ctor_m4D77CEC8F91682A2D9492AE815F89B178BF9717D,
+	Menu_Start_m50A1B94A03D9E9BE1E2B69E931A1E02943BB2F36,
+	Menu_Update_mDF23A59FAEDA72A8B115C4C3CF2ACCAFC6B415AD,
+	Menu__ctor_m7EE9043A2E9DD126B6F69DD8AE3BC9CBDF6E2134,
+	ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m0AAD92A3D682C1DFC1B5630585550B6CAFBAFE32,
+	ARFeatheredPlaneMeshVisualizer_set_featheringWidth_m4761377095817FA81908BC43FD08EF600F6CC231,
+	ARFeatheredPlaneMeshVisualizer_Awake_m7FD9AC4C30D2F12A0A30CC3CAD9EC6C17B07C45D,
+	ARFeatheredPlaneMeshVisualizer_OnEnable_mEE0AFFC028F876DE841312FEB8CA15FEAD105FE1,
+	ARFeatheredPlaneMeshVisualizer_OnDisable_m51213AC9D2DBD97C93CC41330F65D74661D5DC81,
+	ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_m354D58D0B29B40FE6D3A6689590E271D9282323F,
+	ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_m241EDA47E1C4DECA8CCBC61D8DF6EC300546CDF0,
+	ARFeatheredPlaneMeshVisualizer__ctor_mA27A238E88D5FBA54D4DDFE588080DD41B449A4C,
+	ARFeatheredPlaneMeshVisualizer__cctor_mF5C9F857AC060614B46B9359B3F0859FBC309DF3,
+	GPSLocation_Start_m11D560FBC61B4B9D9A3BB6253E16E8F1004E3824,
+	GPSLocation_GPSLoc_mA47F0A51D66F6D7E585A5A60E6CD8F3E89DE22D3,
+	GPSLocation_UpdateGPSData_m5703907876EFCB4D6C331A346FA90BAFA9FC75A6,
+	GPSLocation__ctor_m6CB6A54C1BC5B56BADEE9FD50DA9F259F834D814,
+	U3CGPSLocU3Ed__7__ctor_m12CD184E0688656F4629F334516458DFB5FC576C,
+	U3CGPSLocU3Ed__7_System_IDisposable_Dispose_m516EF9ED6831C68FA7AD3532B42DDA943C2F37A8,
+	U3CGPSLocU3Ed__7_MoveNext_m01660B82714AFA7A87161FB9686DDD9FB0441349,
+	U3CGPSLocU3Ed__7_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m679A24C73E5A917BC5C9C87C4DD7D26E9244545F,
+	U3CGPSLocU3Ed__7_System_Collections_IEnumerator_Reset_m961E137257682C39EAB056AE53D8D4331E473617,
+	U3CGPSLocU3Ed__7_System_Collections_IEnumerator_get_Current_mA80D190D4ACB35ACFE7D1F6EC23938E156E23189,
+	Interface_OpenMenuScene_mF08DA71F4E4B3535BFD7891D88F3D5445C670296,
+	Interface__ctor_m44EC2832625D427080CE1B16A4731F7FA804C09E,
+	PlaceObjOnPlane_Awake_m45954D39E5753EA730A55B8093179ED7F6023505,
+	PlaceObjOnPlane_Start_mB0F25C113A201B9CE495D542EAAB3FBD91DB8420,
+	PlaceObjOnPlane_Update_mA6218EDE3773A987D2F221993EAAEDC4F67C974A,
+	PlaceObjOnPlane_UpdatePlacementPosition_mCBBA7E82D7C37E2D0AACD59110310FB6BD31D57D,
+	PlaceObjOnPlane_UpdatePlacementIndicator_m00585A1EF770BC41FEB45995D61B36AF58CB3B7E,
+	PlaceObjOnPlane_PlaceObject_m4C114795091B34D32E2EB4F00281B683741DB0E0,
+	PlaceObjOnPlane_SetPrefabType_mC235BB4386C172A157E90D3B4EC88CF294C5D10D,
+	PlaceObjOnPlane_GPSLoc_m9654A549B2B472C38A26D5048D7476214DDB0741,
+	PlaceObjOnPlane_UpdateGPSData_mD6773D3D33C781DB91501E90209E253A794E64F5,
+	PlaceObjOnPlane__ctor_m12D5EB396F3C9B0ABF13A0E3D629A8032474FD7B,
+	PlaceObjOnPlane__cctor_mC8BA14CA1F3CEFD48F1F828807A883A899950EDF,
+	U3CGPSLocU3Ed__19__ctor_mDA9FB57B3E2F51FD82DF33C512580FBA9C05A0DC,
+	U3CGPSLocU3Ed__19_System_IDisposable_Dispose_m1E83268494E311594B45DD3579BB94CC346FC948,
+	U3CGPSLocU3Ed__19_MoveNext_mD2F43B64730C093D38F2A23A766675D29C778B8F,
+	U3CGPSLocU3Ed__19_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m54C179442D11BB1408A4CE9EC05BD6B5DEE75A1A,
+	U3CGPSLocU3Ed__19_System_Collections_IEnumerator_Reset_mD168AD012ADD365412B0110FC686082D77105DC3,
+	U3CGPSLocU3Ed__19_System_Collections_IEnumerator_get_Current_m3AFA995D705CC9476FFC0619A874E396BAA1B486,
+	placeArtefacts_Start_m60C00C300A875FA4D43D440ABC6284CFCBDDE29C,
+	placeArtefacts_Update_mACC4E288299183ACBB44D1571F42F7EC90E1089E,
+	placeArtefacts__ctor_m180BE66B9E59136D9083EC63B3F1D04D0EF8CEB5,
+};
+static const int32_t s_InvokerIndices[50] = 
+{
+	2134,
+	2134,
+	2134,
+	2134,
+	2134,
+	2134,
+	2134,
+	2134,
+	2134,
+	2119,
+	1791,
+	2134,
+	2134,
+	2134,
+	1716,
+	1772,
+	2134,
+	3606,
+	2134,
+	2096,
+	2134,
+	2134,
+	1758,
+	2134,
+	2060,
+	2096,
+	2134,
+	2096,
+	2134,
+	2134,
+	2134,
+	2134,
+	2134,
+	2134,
+	2134,
+	2134,
+	1772,
+	2096,
+	2134,
+	2134,
+	3606,
+	1758,
+	2134,
+	2060,
+	2096,
+	2134,
+	2096,
+	2134,
+	2134,
+	2134,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	50,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
